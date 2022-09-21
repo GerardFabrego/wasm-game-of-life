0b2283ea8a8ae6110c16c735f57e64e8715569cf
gerardfabrego <gerardfabrego@gmail.com>
2022-09-21 09:45:49 +0200

    Init website

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,11 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+extern "C" {
+    fn alert(s: String);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+pub fn greet(name: String) {
+    alert(format!("Hello, {}!", name));
 }
